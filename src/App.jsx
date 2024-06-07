7d237fc316f1bf38f886cf44113c5c1194fadaba
Avijit07x <deyavijit134@gmail.com>
2024-06-07 16:27:38 +0530

    fix : bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 	return (
-		<>
+		<div className="bg-[#f1fcf4]">
 			<ToastContainer
 				position="top-right"
 				autoClose={3000}
@@ -20,10 +20,9 @@ const App = () => {
 				transition="Bounce"
 			/>
 			<ToastContainer />
-			<div className="absolute inset-0 -z-10 h-full w-full bg-[#f1fcf4] bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
 			<Navbar />
 			<Hero />
-		</>
+		</div>
 	);
 };
 
